6840a82b43bea2b395a6e6ea208ce481c47c9d58
dhaead <damion@hawaii.edu>
2015-08-01 12:46:08 -1000

    latest practice sd card
    most recent sd card code

--- sd_practice/sd_practice.ino
+++ sd_practice/sd_practice.ino
@@ -1,20 +1,41 @@
+#include <SD.h>
+#include <SPI.h>
+//SPI Settings
+//MOSI,MISO,SCLK set by default
+int CS_pin=10;
+int pow_pin=8;
+
 void setup() {
   // put your setup code here, to run once:
 
+Serial.begin(9600);
+Serial.println("Initializing Card");
+pinMode(CS_pin, OUTPUT);
 pinMode(pow_pin, OUTPUT);
 digitalWrite(pow_pin,HIGH);
 //check if card ready
 if(!SD.begin(CS_pin))
-
-
-
+  {
+    Serial.println("Card Failed");
+    return;
+  }
+  Serial.println("Card Ready");
 }
-
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
-String dataString = "Hello"
-
+String dataString = "Hello";
+File dataFile = SD.open("log.txt", FILE_WRITE);
+if(dataFile)
+  {
+  dataFile.println(dataString);
+  dataFile.close();
+  }
+  else
+  {
+    Serial.println("couldn't access file");
+  }
 
 //Only one file can be open at a time
-delay(5000)
+delay(5000);
 }
